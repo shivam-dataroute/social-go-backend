fbfb489fbdc0848f4cb1f59bea11378a0a23392a
shivam gour <shivam.gour@data-route.com>
2025-01-07 23:00:07 +0530

    first commit

---
+++ cmd/api/api.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/SHIVAM-GOUR/social_go_app/internal/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	config config
+	store  store.Storage
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+	env  string
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
+func (app *application) mount() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+
+		// POST /v1/posts
+		r.Route("/posts", func(r chi.Router) {
+			r.Post("/", app.createPostHandler)
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.getPostHandler)
+			})
+		})
+	})
+
+	return r
+}
+
+func (app *application) run(mux *chi.Mux) error {
+
+	srv := &http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+	log.Printf("SERVER HAS STARTED AT: %s \n", app.config.addr)
+
+	return srv.ListenAndServe()
+}
