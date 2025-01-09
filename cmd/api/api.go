53a1309a2d07a11c7e3e8a0d68689f0a7f773e8a
shivam gour <shivam.gour@data-route.com>
2025-01-10 00:46:09 +0530

    35 follow and unfollow APIs done

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -55,6 +55,16 @@ func (app *application) mount() *chi.Mux {
 				r.Patch("/", app.updatePostHandler)
 			})
 		})
+
+		r.Route("/users", func(r chi.Router) {
+			r.Route("/{userID}", func(r chi.Router) {
+				r.Use(app.userContextMiddleware)
+				r.Get("/", app.getUserHandler)
+
+				r.Put("/follow", app.followUserHandler)
+				r.Put("/unfollow", app.unfollowUserHandler)
+			})
+		})
 	})
 
 	return r
